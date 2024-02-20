d4c0a0362c9b5704c870b4b7e1c0782f6522f55e
HanSangMin <sorkdhks33@gmail.com>
2024-02-20 17:27:40 +0900

    for jenkins 2

--- myproject.cmd
+++ myproject.cmd
@@ -1,5 +1,5 @@
 @echo off
 set FLASK_APP=test
 set FLASK_DEBUG=true
-set PATH=C:\ITStudy\09_flask\fisa-flask\myproject\Scripts;%PATH%
-C:\ITStudy\09_flask\fisa-flask\myproject\Scripts\activate.bat
+set PATH=C:\ITStudy\09_flask\fisa-flask-2\myproject\Scripts;%PATH%
+C:\ITStudy\09_flask\fisa-flask-2\myproject\Scripts\activate.bat
